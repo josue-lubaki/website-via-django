b653e3a87d4cb2da6b1d61a90995d12b68c503c3
Josue Lubaki <lubaki@uqtr.ca>
2020-12-01 04:56:35 -0500

    implementation subscribe

--- noyau/urls.py
+++ noyau/urls.py
@@ -1,18 +1,3 @@
-"""Mysite URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 # Supprimer la ligne : from django.contrib import admin
 from django.urls import path
 
@@ -21,9 +6,10 @@ from . import views
 
 # Plutard, Quand on voudra créer des urls pour les clients, dynamiquement
 app_name = "noyau"
+
 urlpatterns = [
-    # Ajouter le path à la quelle les requests de "app_name" sera envoyés
     path("", views.homepage, name="homepage"),
-    
-    # Supprimer la ligne : path('admin/', admin.site.urls),
+    path("subscribe/", views.subscribe, name="subscribe"),
+    path("logout/", views.logout_request, name="logout"),
+    path("connexion", views.demandeConnexion, name="connexion"),
 ]
--- noyau/views.py
+++ noyau/views.py
@@ -1,9 +1,61 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Netfeelex
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
+from django.shortcuts import redirect
+from django.contrib import messages
+
 # Create your views here.
 
-
 def homepage(request):
-    return render(request = request, template_name = 'main/home.html',
+    return render(request=request, template_name='main/home.html',
                   context={"netfeelex": Netfeelex.objects.all})
+
+
+def subscribe(request): 
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            # on créer utilisateur
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            login(request, user)
+            return redirect("noyau:homepage")
+        else:
+            for msg in form.error_messages:
+                print(form.error_messages[msg])
+                return render(request, "main/subscribe.html", {"formulaire": form})
+
+    form = UserCreationForm
+    return render(request, "main/subscribe.html", {"formulaire": form})
+
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, "Session est fermé")
+    return redirect("noyau:homepage")
+
+
+def demandeConnexion(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request = request, data = request.POST)
+
+        if form.is_valid():
+            nomUtilisateur = form.cleaned_data.get('username')
+            motDePasse = form.cleaned_data.get('password')
+            user = authenticate(username = nomUtilisateur, password = motDePasse)
+
+            if user is not None:
+                login(request,user)
+                messages.info(request, f"Vous êtes maintenant connecté étant que {nomUtilisateur}")
+                return redirect("/")
+            else:
+                messages.error(request, "Nom d'Utilisateur ou Mot de Passe non valide !")
+        else:
+            messages.error(request, "Nom d'Utilisateur ou Mot de Passe non valide !")
+
+    form = AuthenticationForm()
+    return render(request, "main/connexion.html", {"formulaire": form})
+
